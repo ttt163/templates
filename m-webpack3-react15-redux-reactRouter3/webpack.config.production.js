e4eb2517ab3a3431ad78dd959c906bfdbb7156cb
tantingting <tantingting@xiaomi.com>
2018-11-01 17:09:35 +0800

    m端模版

---
+++ m-webpack3-react15-redux-reactRouter3/webpack.config.production.js
@@ -0,0 +1,142 @@
+const {resolve} = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const StyleLintPlugin = require('stylelint-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+const ROOT_PATH = resolve(__dirname)
+const SRC_PATH = resolve(ROOT_PATH, 'src')
+const DIST_PATH = resolve(ROOT_PATH, 'dist')
+const LIBS_PATH = resolve(ROOT_PATH, 'libs')
+const TEM_PATH = resolve(LIBS_PATH, 'template')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: {
+        index: resolve(SRC_PATH, 'index.jsx'),
+        vendors: [
+            'axios',
+            'babel-polyfill',
+            'react',
+            'react-dom',
+            'react-redux',
+            'react-router',
+            'react-router-redux',
+            'redux',
+            'redux-devtools-extension',
+            'redux-thunk'
+        ]
+    },
+    output: {
+        path: DIST_PATH,
+        filename: 'js/[name]-[hash:8].js',
+        chunkFilename: 'js/[name]-[chunkhash:8].js',
+        publicPath: './'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                include: SRC_PATH,
+                use: [
+                    'babel-loader',
+                    'eslint-loader'
+                ]
+            }, {
+                test: /\.(css|scss)?$/,
+                include: ROOT_PATH,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: [autoprefixer()]
+                            }
+                        },
+                        'sass-loader'
+                    ]
+                })
+            }, {
+                test: /\.(png|jpg|jpeg|gif|svg|svgz)?$/,
+                include: SRC_PATH,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'img/[name]-[hash:8].[ext]?'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.scss']
+    },
+    externals: {
+        zepto: '$',
+        jquery: '$'
+    },
+    plugins: [
+        new StyleLintPlugin(),
+        new UglifyJSPlugin({
+            uglifyOptions: {
+                compress: {
+                    warnings: false,
+                    drop_console: false
+                },
+                output: {
+                    // 最紧凑的输出
+                    beautify: false,
+                    // 删除所有的注释
+                    comments: false,
+                }
+            }
+        }),
+        new CleanWebpackPlugin([DIST_PATH], {
+            root: '',
+            verbose: true,
+            dry: false
+        }),
+        new webpack.ProvidePlugin({
+            $: 'zepto' || 'jquery',
+            zepto: 'zepto',
+            jQuery: 'jquery',
+            'window.zepto': 'zepto',
+            'window.jQuery': 'jquery'
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production'),
+                'BABEL_ENV': JSON.stringify('production')
+            }
+        }),
+        new ExtractTextPlugin({
+            filename: 'css/[name]-[contenthash:8].css',
+            disable: false,
+            allChunks: true
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            filename: 'js/[name]-[hash:8].js',
+            names: ['vendors']
+        }),
+        new HtmlWebpackPlugin({
+            title: '微信商城',
+            keywords: '微信商城',
+            description: '微信商城',
+            filepath: DIST_PATH,
+            template: resolve(TEM_PATH, 'index.html'),
+            chunks: ['index', 'vendors'],
+            filename: 'index.html',
+            inject: 'body'
+        }),
+        new CopyWebpackPlugin([{ from: 'assets', to: 'assets' }])
+    ]
+}
